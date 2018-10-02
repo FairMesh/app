1550a5f6a02e04397a9b45d3b342c8ef54513206
Shiqi <szhao@fairventureslab.ca>
2018-10-02 09:17:56 -0400

    Replace public and src of old dashboard

--- fairmesh-dashboard/src/App.vue
+++ fairmesh-dashboard/src/App.vue
@@ -1,32 +1,54 @@
 <template>
   <div id="app">
-    <Navigation></Navigation>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <router-view/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <PieGraph v-bind:data="ibm_db_data" v-bind:show_pie="!show"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Navigation from './components/Navigation';
-
+import PieGraph from "./components/PieGraph.vue";
+const axios = require("axios");
 export default {
-	name: 'app',
-	components: {
-		HelloWorld: HelloWorld,
-		Navigation: Navigation
-	}
+  name: "app",
+  components: {
+    PieGraph
+  },
+  data: function() {
+    return {
+      ibm_db_data: [],
+      show: false
+    };
+  },
+  methods: {
+    //  getData() {}
+  },
+  mounted: function() {
+    // var temp = ["test"];
+    var self = this;
+    return axios
+      .get("https://fairmesh-dashboard.mybluemix.net/api/getData")
+      .then(function(resp) {
+        console.log("Response", resp);
+        self.show = true;
+        self.ibm_db_data = resp.data;
+      })
+      .catch(function(error) {
+        console.log("noooo issa error tingg fam");
+        console.log(error);
+      });
+    // window.setInterval(() => {
+    // 	this.show = !this.show;
+    // }, 1000);
+  }
 };
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
